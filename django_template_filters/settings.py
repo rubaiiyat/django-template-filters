df95ff8ca1c96230445e404d097f77eeaa1bb5bf
rubaiiyat <abirr1516@gmail.com>
2025-12-25 21:52:58 +0600

    add static file inside the project and inside app

--- django_template_filters/settings.py
+++ django_template_filters/settings.py
@@ -1,3 +1,4 @@
+from django.contrib import staticfiles
 """
 Django settings for django_template_filters project.
 
@@ -116,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [
+    BASE_DIR/'static'
+]
