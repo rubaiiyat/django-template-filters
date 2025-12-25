4b4ebc5502dc6508d51422fe5e4f5cedc7043d19
rubaiiyat <abirr1516@gmail.com>
2025-12-25 16:17:39 +0600

    work with slugify

--- app/views.py
+++ app/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def templates(request):
-    return render(request,'templates_filters.html')
+    user={'name':'John Doe', 'age':30, 'city':'New York','lst':{'python','django','html','css','javascript'},'courses':[{'id':101,'name':'Python Basics','price':10,'img':'https://dotnettrickscloud.blob.core.windows.net/article/5520250715143827.webp'},{'id':102,'name':'C++ basic to advanced','price':20,'img':'https://blog.jetbrains.com/wp-content/uploads/2023/03/Blog_Social_share_image_1280x720-8.png'},{'id':103,'name':'Basics of modern javascript','price':15,'img':'https://miro.medium.com/1*ChC-1PWD1vEXBwV_YRrYWw.jpeg'}]}
+    return render(request,'templates_filters.html',user)
 
 def about(request):
     return render(request,'about.html')
